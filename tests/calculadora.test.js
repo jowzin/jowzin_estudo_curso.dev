730cfbf5d9edb63d11dbe840c5d35dcfc7e16053
JowzinM <jmmreis3@gmail.com>
2026-06-26 19:01:55 -0300

    Refactor tests in calculadora.test.js to utilize the calculadora model for addition operations; add new test cases for various scenarios including error handling.

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,9 +1,19 @@
+const caculadora = require("../models/calculadora");
+
 test("soma de 2 + 2 deve ser 4", () => {
-  console.log("calculando 4 ...");
-  expect(2 + 2).toBe(4);
+  const resultado = caculadora.somar(2, 2);
+  // console.log("resultado: ", resultado);
+  expect(resultado).toBe(4);
 });
 
-test("soma de 5 + 5 deve ser 10", () => {
-  console.log("calculando 10 ...");
-  expect(5 + 5).toBe(10);
+test("soma de 5 + 100 deve ser 105", () => {
+  const resultado = caculadora.somar(5, 100);
+  // console.log("resultado: ", resultado);
+  expect(resultado).toBe(105);
+});
+
+test("soma de 'banana' + 100 deve ser 'Erro'", () => {
+  const resultado = caculadora.somar("banana", 100);
+  // console.log("resultado: ", resultado);
+  expect(resultado).toBe("Erro");
 });
