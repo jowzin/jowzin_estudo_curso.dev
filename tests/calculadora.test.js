e347d4bd914756566c164308f8c45ed1525050c4
JowzinM <jmmreis3@gmail.com>
2026-06-26 18:39:54 -0300

    Update Home component with new features and improve test coverage; refine README for better guidance on testing procedures.

---
+++ tests/calculadora.test.js
@@ -0,0 +1,9 @@
+test("soma de 2 + 2 deve ser 4", () => {
+  console.log("calculando 4 ...");
+  expect(2 + 2).toBe(4);
+});
+
+test("soma de 5 + 5 deve ser 10", () => {
+  console.log("calculando 10 ...");
+  expect(5 + 5).toBe(10);
+});
