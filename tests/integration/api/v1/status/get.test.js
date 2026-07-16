1fca135d6e57a157a6508bf622d4874848d20ad4
JowzinM <jmmreis3@gmail.com>
2026-07-16 17:31:16 -0300

    Add local development comment in .env.development and enable logging in status API test

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,5 +10,5 @@ test("GET /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-  // console.log(responseBody);
+  console.log(responseBody);
 });
